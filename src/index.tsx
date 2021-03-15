4d1cb279e9985ed4e684aaf28c316f2c4c8b52b2
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-15 15:12:40 +0100

    Introduce lazy loading

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import { PiletApi } from 'piral-tryout';
 import MovieTile from './components/MovieTile';
 import { ToggleProps } from './models/proptypes';
 import './style.scss';
-import WatchPage from './components/WatchPage';
 
 export function setup(app: PiletApi) {
   const Toggle: React.FC<ToggleProps> = props => <app.Extension name="ListToggle" params={props}></app.Extension>;
   app.registerExtension('MovieTile', props => <MovieTile {...props.params} Toggle={Toggle}></MovieTile>);
 
+  const WatchPage = React.lazy(() => import('./components/WatchPage'));
   app.registerPage('/watch/:media_type/:video_id', WatchPage);
 }
