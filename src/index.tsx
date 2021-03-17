913de4aeea8276bdef7b23cce3bb926362bef8d0
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-17 11:20:26 +0100

    Update references

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PiletApi } from 'piral-tryout';
+import { PiletApi } from 'netflix-piral';
 import MovieTile from './components/MovieTile';
 import { ToggleProps } from './models/proptypes';
 import './style.scss';
